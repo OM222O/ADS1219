a7f4292f3919552bba8cd29f810566a9342408ff
OM222O <32412692+OM222O@users.noreply.github.com>
2018-12-29 22:01:31 +0330

    Add files via upload

---
+++ examples/ADS1219/ADS1219.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <ADS1219.h>
+#define address 0x40
+#define rst 14
+#define drdy 15
+
+ADS1219 ads(drdy);
+
+
+
+void setup() {
+  ads.begin();
+  pinMode(drdy,INPUT_PULLUP);
+  pinMode(rst,OUTPUT);
+  digitalWrite(rst,HIGH);
+  Wire.begin();
+  Serial.begin(9600);
+  ads.setVoltageReference(REF_EXTERNAL);
+}
+
+void loop() {
+  Serial.println("Single ended result:");
+  Serial.println(ads.readSingleEnded(2)*2.048/pow(2,23),5);
+  delay(2000);
+  Serial.println("Differential result:");
+  Serial.println(ads.readDifferential_2_3()*2.048/pow(2,23),5);
+  delay(2000);
+}
